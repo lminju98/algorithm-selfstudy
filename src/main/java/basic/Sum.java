1ddb3694fcaf0cbff81a05d9dd17d7caae5e57e1
lminju98 <bdscj2@naver.com>
2024-06-15 15:27:58 +0900

    A+B

---
+++ src/main/java/basic/Sum.java
@@ -0,0 +1,14 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a + b);
+    }
+}
