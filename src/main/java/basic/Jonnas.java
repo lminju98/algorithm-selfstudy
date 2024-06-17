4bb36a3d4a0d041a995ff27d152334ed827d3a5b
lminju98 <bdscj2@naver.com>
2024-06-17 18:31:59 +0900

    joonas

---
+++ src/main/java/basic/Jonnas.java
@@ -0,0 +1,28 @@
+package basic;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ */
+
+public class Jonnas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.next();
+
+        String getName =getName(name);
+        System.out.println(getName);
+    }
+    public static String getName(String n){
+        String name = n ;
+
+            if(name.equals("joonas")){
+                return (name + "??!");
+            } else {
+                return name;
+    }
+}
+}
