2fb215d87aaa26a82393d918eeccdc971005e11a
lminju98 <bdscj2@naver.com>
2024-06-17 17:25:47 +0900

    a*b

---
+++ src/main/java/basic/Multiply.java
@@ -0,0 +1,19 @@
+package basic;
+
+import java.util.Scanner;
+
+
+/**
+ * A*B
+ */
+
+public class Multiply {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a * b);
+    }
+}
