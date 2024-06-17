e38a0c218572acf8b8c5c8399a6a5e8c83cbb1da
lminju98 <bdscj2@naver.com>
2024-06-17 17:41:47 +0900

    operations

---
+++ src/main/java/basic/Division.java
@@ -0,0 +1,14 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Division {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float a = scanner.nextFloat();
+        float b = scanner.nextFloat();
+
+        System.out.println(a / b);
+    }
+}
---
+++ src/main/java/basic/Operations.java
@@ -0,0 +1,19 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Operations {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
