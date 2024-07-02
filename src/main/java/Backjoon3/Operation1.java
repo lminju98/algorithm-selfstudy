9abc4556b655234da24d3daaaf36408e45ae4841
lminju98 <bdscj2@naver.com>
2024-07-02 17:51:25 +0900

    20240702

---
+++ src/main/java/Backjoon3/Operation1.java
@@ -0,0 +1,18 @@
+package Backjoon3;
+
+import java.util.Scanner;
+
+public class Operation1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i=1;i<10;i++){
+            System.out.println(n + " * " + i + " = " +n*i);
+        }
+
+    }
+
+}
+
